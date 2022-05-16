4a55438b1cbd541b99c8fac4c0cf2652873b2753
hack0 <ivan.golubev.1@mail.ru>
2022-05-16 21:52:24 +0300

    Удалены лишние, недоработанные функции. Изменена функция "List".

--- main.py
+++ main.py
@@ -173,24 +173,8 @@ def gui_users():
 def get_users():
     users = ""
     for i in collections.find({}):
-        users += str(i)
+        users += "| Имя: {}, Возраст: {}, Зарплата: {}, Ранг: {}.".format(i['personal_data'], i['age'], i['salary'], i['rank'])
         users += "\n"
-    return users
-
-
-def guicount_users():
-    win = Toplevel(root)
-    win.geometry("1000x500")
-
-    # txt41 = Label(win, text=[listt for listt in collections.find({})])
-    txt41 = Label(win, text=count_users())
-    txt41.pack()
-
-
-def count_users():
-    users = 0
-    for i in collections.find({}):
-        users += 1
     return users
 
 
@@ -218,49 +202,6 @@ def info():
     fio = str(e41.get())
     status1.configure(text=collections.find_one({'personal_data': fio}))
 
-# SalaryManager
-def clicksredn():
-     win = Toplevel(root)
-     out = Label(win, text=sredn_salary())
-     out.pack()
-
-def sredn_salary():
-    allusers = 0
-    for x in collections.find({}):
-        allusers += 1
-    sum_ = 0
-    for i in collections.find({}):
-       sum_ += int(i['salary'])
-    return sum_ // allusers
-
-def clicksum():
-    win = Toplevel(root)
-    out = Label(win, text=sum_salary())
-    out.pack()
-
-def sum_salary():
-    sum_salary_ = 0
-    for i in collections.find({}):
-        sum_salary_ += int(i['salary'])
-    return sum_salary_
-
-def clickmax_and_min():
-    win = Toplevel(root)
-    out = Label(win, text=max_and_min())
-    out.pack()
-
-def max_and_min():
-    salary = []
-    for i in collections.find({}):
-        salary.append(int(i['salary']))
-        maximum = max(salary)
-        minimum = min(salary)
-    return {'max': maximum, 'min': minimum}
-
-
-def createbackup():
-    backup.add()
-
 
 root = Tk()
 root.geometry("1000x500")
@@ -270,26 +211,13 @@ current_db = db_client[config.db_name]
 collections = current_db[config.collection_name]
 
 
-insert_ = Button(root, text="✘ Add", command=guiinsert)
-edit_ = Button(root, text="✘ Edit", command=guiedit)
-del_ = Button(root, text="✘ Del", command=guiremove)
-getusers_ = Button(root, text="✘ List", command=gui_users)
-info_ = Button(root, text="✘ Info", command=guiinfo)
-sredn_salary_ = Button(root, text="✘ Srednaya Salary", command=clicksredn)
-sum_salary_ = Button(root, text="✘ Sum Salary", command=clicksum)
-max_and_min_ = Button(root, text="✘ Max and Min", command=clickmax_and_min)
-createbackup_ = Button(root, text="✘ Create Backup", command=createbackup)
-count_ = Button(root, text="✘ Count Users", command=guicount_users)
+insert_ = Button(root, text="Add", command=guiinsert)
+edit_ = Button(root, text="Edit", command=guiedit)
+del_ = Button(root, text="Del", command=guiremove)
+getusers_ = Button(root, text="List", command=gui_users)
+info_ = Button(root, text="Info", command=guiinfo)
 insert_.pack()
 edit_.pack()
 del_.pack()
 getusers_.pack()
-info_.pack()
-sredn_salary_.pack()
-sum_salary_.pack()
-max_and_min_.pack()
-count_.pack()
-createbackup_.pack()
-
-
 root.mainloop()
